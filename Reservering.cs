b32017a251918982a108254c7023096e60d7e2e6
valenciomuskiet <94371531+valenciomuskiet@users.noreply.github.com>
2022-06-07 11:52:46 +0200

    Add files via upload

---
+++ Reservering.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace HetDepotApplication
+{
+	public class Reservering
+	{
+		public int Code { get; set; }
+		public string Tijd { get; set; }
+
+		public Reservering()
+		{
+		}
+	}
+}
+
