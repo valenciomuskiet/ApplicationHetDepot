61ceac5ff8abb916aabf0a15eac0e8bdf865ec48
valenciomuskiet <94371531+valenciomuskiet@users.noreply.github.com>
2022-06-12 13:21:15 +0200

    Create reservering12.cs

---
+++ reservering12.cs
@@ -0,0 +1,88 @@
+using System;
+namespace HetDepotApplication
+{
+	public class Reservering11uur
+	{
+		public int Code { get; set; }
+		public string Tijd { get; set; }
+
+
+        public override string ToString()
+        {
+            return String.Format("Student Information:\n\tCode: {0}, Tijd: {1} ", Code, Tijd);
+        }
+    }
+
+    public class Reservering12uur
+    {
+        public int Code { get; set; }
+        public string Tijd { get; set; }
+
+
+        public override string ToString()
+        {
+            return String.Format("Student Information:\n\tCode: {0}, Tijd: {1} ", Code, Tijd);
+        }
+    }
+
+    public class Reservering13uur
+    {
+        public int Code { get; set; }
+        public string Tijd { get; set; }
+
+
+        public override string ToString()
+        {
+            return String.Format("Student Information:\n\tCode: {0}, Tijd: {1} ", Code, Tijd);
+        }
+    }
+
+    public class Reservering14uur
+    {
+        public int Code { get; set; }
+        public string Tijd { get; set; }
+
+
+        public override string ToString()
+        {
+            return String.Format("Student Information:\n\tCode: {0}, Tijd: {1} ", Code, Tijd);
+        }
+    }
+
+    public class Reservering15uur
+    {
+        public int Code { get; set; }
+        public string Tijd { get; set; }
+
+
+        public override string ToString()
+        {
+            return String.Format("Student Information:\n\tCode: {0}, Tijd: {1} ", Code, Tijd);
+        }
+    }
+
+    public class Reservering16uur
+    {
+        public int Code { get; set; }
+        public string Tijd { get; set; }
+
+
+        public override string ToString()
+        {
+            return String.Format("Student Information:\n\tCode: {0}, Tijd: {1} ", Code, Tijd);
+        }
+    }
+
+    public class Reservering17uur
+    {
+        public int Code { get; set; }
+        public string Tijd { get; set; }
+
+
+        public override string ToString()
+        {
+            return String.Format("Student Information:\n\tCode: {0}, Tijd: {1} ", Code, Tijd);
+        }
+    }
+}
+
