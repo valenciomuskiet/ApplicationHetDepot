f68c4209216210f005acf6891f061af63f1a6657
valenciomuskiet <94371531+valenciomuskiet@users.noreply.github.com>
2022-06-14 00:23:47 +0200

    Create Reservering14.cs

---
+++ Reservering14.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace HetDepotApplication
+{
+    public class Reservering                                                    /// Class voor reservering
+    {
+        public int code;
+        public DateTime tijd;
+
+        public Reservering(int Acode, DateTime Atijd)                            ///Constructor 
+        {
+            code = Acode;
+            tijd = Atijd;
+        }
+    }
+}
